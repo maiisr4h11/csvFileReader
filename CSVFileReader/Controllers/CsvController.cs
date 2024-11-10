149e86c6f8c2fb6948da23c2dc542dc6d6250b32
maiisr4h11 <maisarahibrahim2511@gmail.com>
2024-11-10 11:50:32 +0800

    add

---
+++ CSVFileReader/Controllers/CsvController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using CsvHelper;
+using CSVFileReader.Models;
+
+
+namespace CSVFileReader.Controllers
+{
+
+    public class CsvController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UploadCsv(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return Content("File not selected");
+
+            var csvData = new List<CsvData>();
+
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<CsvData>();
+                csvData.AddRange(records);
+            }
+
+            return View(csvData);
+        }
+    }
+
+
+}
---
+++ CSVFileReader/Models/CsvData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSVFileReader.Models
+{
+    public class CsvData
+    {
+        public string Company { get; set; }
+        public DateTime CeCreated { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Project { get; set; }
+        public string QuoNumber { get; set; }
+        public string QuoTitle { get; set; }
+        public string ItemServiceCode { get; set; }
+        public string ItemService { get; set; }
+        public string Description { get; set; }
+        public string SubBrandCode { get; set; }
+        public string SubBrand { get; set; }
+        public string AgentCode { get; set; }
+        public string Agent { get; set; }
+        public string CustomerCode { get; set; }
+        public string Customer { get; set; }
+        public DateTime DetailingSamplingDay { get; set; }
+        public int Quantity { get; set; }
+        public int TotalQuantity { get; set; }
+        public decimal RateUnit { get; set; }
+        public decimal Price { get; set; }
+        public decimal SST { get; set; }
+        public decimal Total { get; set; }
+        public string TaxCode { get; set; }
+    }
+
+}
